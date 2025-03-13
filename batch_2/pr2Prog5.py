d46f083aabf0cdd93b9b3a0399fcb311b8a8f9a8
Jaydie <johndenieldomalaon@gmail.com>
2025-03-13 22:33:29 +0800

    changed the variables

---
+++ batch_2/pr2Prog5.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first num: "))
+num2 = int(input("Enter second num: "))
+
+if num1 !=0:
+    remainder = num1 % num2
+    print("Remainder: ", remainder)
+else:
+    print("Error. Not Possible")
