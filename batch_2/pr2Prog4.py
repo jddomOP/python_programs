024958a7725b28638ba81f21532ed3a3bbb7fb4b
Jaydie <johndenieldomalaon@gmail.com>
2025-03-13 22:32:27 +0800

    changed the variables

---
+++ batch_2/pr2Prog4.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first num: "))
+num2 = int(input("Enter second num: "))
+
+if num1 != 0:
+    quotient = num1 // num2
+    print("Quotient without decimal: ", quotient)
+else:
+    print("Error. Not possible")
